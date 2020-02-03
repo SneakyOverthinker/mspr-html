4c710bf6d1bb63c3f7383520909701c50a5a03e7
Rebecca <rebecca.colombel@hotmail.fr>
2020-02-03 15:38:11 +0100

    update: navbar links

--- includes/header.php
+++ includes/header.php
@@ -21,16 +21,19 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="/mspr-html/home.php">Accueil<span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
+                        <a class="nav-link" href="/mspr-html/catalogue.php">Les loutres</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
+                        <a class="nav-link" href="/mspr-html/refuge.php">Le refuge</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        <a class="nav-link" href="/mspr-html/contact.php">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/mspr-html/about.php">A propos</a>
                     </li>
                 </ul>
             </div>
