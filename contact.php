edfe6b55acbebd77e8a5be69416b129f1b2576cc
tam444 <tami.sgnrt@gmail.com>
2020-02-03 17:19:27 +0100

    Auto stash before merge of "master" and "origin/master"

--- contact.php
+++ contact.php
@@ -1,5 +1,31 @@
 <?php require_once 'includes/header.php'; ?>
-    <section id="home-jumbotron"></section>
-    LA MAISON
+    <section id="contact">
+        <div class="container">
+            <form action="" method="POST">
+                <div class="form-group">
+                    <label for="first_name">Prénom</label>
+                    <input type="text" id="first_name" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label for="last_name">Nom</label>
+                    <input type="text" id="last_name" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="text" id="email" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label for="message">Message</label>
+                    <textarea id="message" class="form-control"> </textarea>
+                </div>
+                <button class="btn2">
+                    Envoyer
+                </button>
+            </form>
+        </div>
+
 
 <?php require_once 'includes/footer.php'; ?>
