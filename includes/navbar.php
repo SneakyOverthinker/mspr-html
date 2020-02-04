4526885685a4c7074dd8daa9e9d528ad64e8e0ff
Rebecca <rebecca.colombel@hotmail.fr>
2020-02-04 14:52:41 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ includes/navbar.php
@@ -0,0 +1,28 @@
+<header id="header">
+    <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+        <a class="navbar-brand" href="./">OTTERS</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item ">
+                    <a class="nav-link" href="./">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="catalogue">Les loutres</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="refuge">Le refuge</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about">A propos</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
