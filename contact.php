c9cde8ef9e6788ee8a6fdb73f44414876df8abb1
tam444 <tami.sgnrt@gmail.com>
2020-02-04 10:35:19 +0100

    update contact

--- contact.php
+++ contact.php
@@ -1,31 +1,107 @@
 <?php require_once 'includes/header.php'; ?>
+
+
+
     <section id="contact">
+
         <div class="container">
-            <form action="" method="POST">
-                <div class="form-group">
-                    <label for="first_name">Prénom</label>
-                    <input type="text" id="first_name" class="form-control">
-                </div>
+        <!--Section: Contact v.2-->
+        <section class="mb-4">
 
-                <div class="form-group">
-                    <label for="last_name">Nom</label>
-                    <input type="text" id="last_name" class="form-control">
-                </div>
+            <!--Section heading-->
+            <h2 class="h1-responsive font-weight-bold text-center my-4">Contactez-nous</h2>
+            <!--Section description-->
+            <p class="text-center w-responsive mx-auto mb-5">Si vous avez la moindre question contactez-nous ici ou par mail. Nous vous répondrons dans les plus brefs délais.</p>
 
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input type="text" id="email" class="form-control">
-                </div>
+            <div class="row">
 
-                <div class="form-group">
-                    <label for="message">Message</label>
-                    <textarea id="message" class="form-control"> </textarea>
+                <!--Grid column-->
+                <div class="col-md-9 mb-md-0 mb-5">
+                    <form id="contact-form" name="contact-form" action="mail.php" method="POST">
+
+                        <!--Grid row-->
+                        <div class="row">
+
+                            <!--Grid column-->
+                            <div class="col-md-6">
+                                <div class="md-form mb-0">
+                                    <input type="text" id="name" name="name" class="form-control">
+                                    <label for="name" class="">Votre nom</label>
+                                </div>
+                            </div>
+                            <!--Grid column-->
+
+                            <!--Grid column-->
+                            <div class="col-md-6">
+                                <div class="md-form mb-0">
+                                    <input type="text" id="email" name="email" class="form-control">
+                                    <label for="email" class="">Votre e-mail</label>
+                                </div>
+                            </div>
+                            <!--Grid column-->
+
+                        </div>
+                        <!--Grid row-->
+
+                        <!--Grid row-->
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="md-form mb-0">
+                                    <input type="text" id="subject" name="subject" class="form-control">
+                                    <label for="subject" class="">Objet de votre message</label>
+                                </div>
+                            </div>
+                        </div>
+                        <!--Grid row-->
+
+                        <!--Grid row-->
+                        <div class="row">
+
+                            <!--Grid column-->
+                            <div class="col-md-12">
+
+                                <div class="md-form">
+                                    <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
+                                    <label for="message">Votre message</label>
+                                </div>
+
+                            </div>
+                        </div>
+                        <!--Grid row-->
+
+                    </form>
+
+                    <div class="text-center text-md-left">
+                        <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Envoyer</a>
+                    </div>
+                    <div class="status"></div>
                 </div>
-                <button class="btn2">
-                    Envoyer
-                </button>
-            </form>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-md-3 text-center">
+                    <ul class="list-unstyled mb-0">
+                        <li><i class="fas fa-map-marker-alt fa-2x"></i>
+                            <p>Nantes, 44200, France</p>
+                        </li>
+
+                        <li><i class="fas fa-phone mt-4 fa-2x"></i>
+                            <p>+ 06 98 37 67 46</p>
+                        </li>
+
+                        <li><i class="fas fa-envelope mt-4 fa-2x"></i>
+                            <p>otters@gmail.com</p>
+                        </li>
+                    </ul>
+                </div>
+                <!--Grid column-->
+
+            </div>
+        </section>
         </div>
+    </section>
 
+
+   <!--Section: Contact v.2-->
 
 <?php require_once 'includes/footer.php'; ?>
