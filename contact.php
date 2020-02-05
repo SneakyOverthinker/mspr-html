6c2e9b42e57100c9f0b1ba8c2b53b7d308b7b897
tam444 <tami.sgnrt@gmail.com>
2020-02-05 14:56:24 +0100

    update

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <?php require_once 'includes/header.php'; ?>
 
     <section id="jumbotron"
-             style=" background-image: url('https://static.lpnt.fr/images/2018/12/21/17792283lpw-17792282-article-jpg_5825528.jpg');">
+             style=" background-image: url('https://static.lpnt.fr/images/2018/12/21/17792283lpw-17792282-article-jpg_5825528.jpg'); background-repeat: no-repeat; background-position: center center; background-size: cover; padding: 220px">
         <div class="container">
             <!--Grid row-->
             <h1 class="display-4 text-white">Ma page</h1>
