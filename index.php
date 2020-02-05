e9934112f385bd5146cd34190df5edfbbad30525
Rebecca <rebecca.colombel@hotmail.fr>
2020-02-05 15:35:44 +0100

    update : scss

--- index.php
+++ index.php
@@ -5,20 +5,35 @@
         <!-- Full Page Intro -->
         <div class="container">
             <!--Grid row-->
-                <h1 class="display-4 text-white">Ma page</h1>
+                <h1 class="display-4 text-white">OTTERS</h1>
             <!--Grid row-->
         </div>
     </section>
     <!-- Main navigation -->
 
     <!--Main Layout-->
-    <main>
+    <main id="main">
+        <div class="container">
+
+                        <span>
+                            bla bla bla
+                        </span>
+
+            <h1>
+                lorem ipsum
+
+            </h1>
+
+            <div class="container">
+                stuff stuff stuff
+            </div>
         <div class="container">
             <!--Grid row-->
             <div class="row py-5">
                 <!--Grid column-->
 
-                <div class="col text-center">
+
+                <div class="col-md-6 text-center">
                     <div class="card card-body">
 
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
@@ -28,9 +43,13 @@
                         non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     </div>
                 </div>
-                <div class="col">
+                <div class="col-md-6">
                     <div class="card card-body">
-                        blabla
+
+                        <div class="card">
+                        <img src="public/images/loutreindex.png">
+
+
                     </div>
                 </div>
                 <!--Grid column-->
