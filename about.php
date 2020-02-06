f8b5efef74e99aa6c81f548c1e68c09d4024be80
tam444 <tami.sgnrt@gmail.com>
2020-02-06 15:27:17 +0100

    update about

--- about.php
+++ about.php
@@ -40,13 +40,49 @@
                                 <p class="card-text">Famille d'accueil des loutres, elle a sauvé Manuel qui est
                                     maintenant
                                     disponible à l'adoption.</p>
-                            </div>
+                                </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
+                <div class="container py-5">
+                    <div class="text-align-center">
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="public/images/loutre1" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="public/images/loutre2" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="public/images/loutre3" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="public/images/loutre4" class="d-block w-100" alt="...">
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
+            </div>
+            </div>
         </div>
     </section>
+
+
+
 
 <?php require_once 'includes/footer.php'; ?>
